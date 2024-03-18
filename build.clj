a03ce77fd814a01368d0da54069c5db7798f63b8
eve n.u <eve@eatbigger.fish>
2024-03-18 15:09:33 -0700

    build: Adopt tools.build for script interfaces
    This sets up future changes that use `clj-nix`
    for reproducible dependency resolution and nix
    packaging flows.

---
+++ build.clj
@@ -0,0 +1,26 @@
+(ns build
+  (:require [clojure.tools.build.api :as build]))
+
+(def lib 'io.github.inferenceql/inferenceql.query)
+(def version (format "1.2.%s" (build/git-count-revs nil)))
+(def class-dir "target/classes")
+(def uber-file (format "target/%s-%s-standalone.jar" (name lib) version))
+
+;; delay to defer side effects (artifact downloads)
+(def basis (delay (build/create-basis {:project "deps.edn"})))
+
+;; clean build artifacts (excludes test artifacts)
+(defn clean [_]
+  (build/delete {:path "target"}))
+
+(defn uber [_]
+  (clean nil)
+  (build/copy-dir {:src-dirs ["src" "resources"]
+                   :target-dir class-dir})
+  (build/compile-clj {:basis @basis
+                      :ns-compile '[inferenceql.query.main]
+                      :class-dir class-dir})
+  (build/uber {:class-dir class-dir
+               :uber-file uber-file
+               :basis @basis
+               :main 'inferenceql.query.main}))
