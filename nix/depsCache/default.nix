f3e1314f7c652696278fb44b89384ad89cce4e0b
eve n.u <eve@eatbigger.fish>
2024-04-01 16:28:40 -0700

    build: include clojure home configuration in devshell

---
+++ nix/depsCache/default.nix
@@ -0,0 +1,9 @@
+{ stdenv,
+  pkgs,
+  mk-deps-cache,
+}: mk-deps-cache {
+    lockfile = builtins.path {
+      path = ./../../deps-lock.json;
+      name = "inferenceql.query.deps-lock.json";
+    };
+  }
