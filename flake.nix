5b275f36ab8b1c63dde9f3e44b338dc9b156f08f
eve n.u <eve@eatbigger.fish>
2024-04-17 16:51:06 -0700

    build: enable opt-in readonly deps

--- flake.nix
+++ flake.nix
@@ -58,6 +58,35 @@
 
           shellHook = ''
             echo "Setting up default dev shell..."
+
+            DEPS_CACHE_TMP="/tmp/clojure_cache_gensql_query"
+            echo "Using read-write clojure cache at:"
+            echo "$DEPS_CACHE_TMP"
+
+            cache_signature_file="/tmp/clojure_cache_gensql_query_signature"
+            cache_signature="$(echo ${depsCache} | tr -d '\n')"
+            last_cache_signature="$(cat $cache_signature_file | tr -d '\n')"
+
+            if [[ $cache_signature != $last_cache_signature ]]; then
+              echo -n $cache_signature > $cache_signature_file
+
+              mkdir -p $DEPS_CACHE_TMP
+              echo "Copying readonly clojure cache to read-write location"
+              rsync -LrltgoD --chmod=ug+w ${depsCache}/ $DEPS_CACHE_TMP/
+            fi
+
+            export CLJ_CONFIG="$DEPS_CACHE_TMP/.clojure"
+            export GITLIBS="$DEPS_CACHE_TMP/.gitlibs"
+            export JAVA_TOOL_OPTIONS="-Duser.home=$DEPS_CACHE_TMP"
+          '';
+        };
+
+        # development shell with readonly dependencies
+        devShells.strict = pkgs.mkShell {
+          buildInputs = [ pkgs.openjdk21 pkgs.clojure pkgs.babashka depsCache ] ++ (basicToolsFn pkgs);
+
+          shellHook = ''
+            echo "Setting up strict dev shell with readonly clojure deps..."
             export CLJ_CONFIG="${depsCache}/.clojure"
             export GITLIBS="${depsCache}/.gitlibs"
             export JAVA_TOOL_OPTIONS="-Duser.home=${depsCache}"
